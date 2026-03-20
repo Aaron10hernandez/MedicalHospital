608cd13e36f287717986053f58a3d15a752ba2cd
PuntoAtoon <aarondh.10@gmail.com>
2026-03-20 18:03:36 -0500

    commit inicial

---
+++ src/main/java/com/mycompany/medicalhospital/ClinicalHospital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.medicalhospital;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class ClinicalHospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
