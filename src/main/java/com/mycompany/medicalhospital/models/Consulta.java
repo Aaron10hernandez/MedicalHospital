2088ef2f4d13db7753850be77686dcf600060919
PuntoAtoon <aarondh.10@gmail.com>
2026-03-20 18:48:13 -0500

    final de la clase 20/03

---
+++ src/main/java/com/mycompany/medicalhospital/models/Consulta.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.medicalhospital.models;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Consulta {
+    private String fecha;
+    private String motivo;
+    private String diagnostico;
+
+    public Consulta() {
+    }
+    
+    
+}
---
+++ src/main/java/com/mycompany/medicalhospital/models/Hospital.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.medicalhospital.models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Hospital {
+    private String nombre;
+    private String ciudad;
+    private ArrayList<Consulta> consultas;
+
+    public Hospital() {
+    }
+
+    public Hospital(String nombre, String ciudad) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public ArrayList<Consulta> getConsultas() {
+        return consultas;
+    }
+
+    public void setConsultas(ArrayList<Consulta> consultas) {
+        this.consultas = consultas;
+    }
+    
+    public Consulta registrarConsulta(){
+        
+    }
+            
+}
